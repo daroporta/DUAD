52ff1dfa1c1c94c1f5ba7eeafae0bdaae4c3d48f
Darwin Oporta <oportadarwin24@gmail.com>
2024-12-03 19:51:09 -0600

    Exercise 3 and Exercise 4 updated based on feedback

--- Semana15/exercise_3.py
+++ Semana15/exercise_3.py
@@ -2,13 +2,13 @@
 
 def bubble_sort(list):   # O(1)
     for amount_run in range(len(list)):    # O(N)
-        Sorted=True                        # O(1)
+        sorted=True                        # O(N)
         for current_position in range(len(list)-amount_run-1):  # O(n^2)
-            if list[current_position] > list[current_position+1]:   # O(1)
-                list[current_position], list[ current_position+1]=list[current_position+1], list[current_position]   # O(1)
-                Sorted= False   # O(1)
-        if Sorted:   # O(1)
-            break    # O(1)
+            if list[current_position] > list[current_position+1]:   # O(n^2)
+                list[current_position], list[ current_position+1]=list[current_position+1], list[current_position] # O(n^2)
+                sorted= False   # O(n^2)
+        if sorted:   # O(N)
+            break    # O(N)
     return list      # O(1)
 
 list=[2,5,6,8,2,9,11,-78,23.6, 3, 12.5]   # O(1)
@@ -16,3 +16,4 @@ print(bubble_sort(list))                  # O(1)
 
 #BIG O NOTATION WORST SCENARIO  O(n^2)
 #Best scenario  O(1)
+
--- Semana15/exercise_4.py
+++ Semana15/exercise_4.py
@@ -2,10 +2,10 @@
 
 def print_numbers_times_2(numbers_list):
 	for number in numbers_list: #O(n)
-		print(number * 2)       # O(1)
+		print(number * 2)       # O(n)
 		
 #Worst scenario: O(n)
-#Best scenario: O(1)
+#Best scenario: O(n)
 		
 
 #check_if_lists_have_an_equal
@@ -13,8 +13,8 @@ def print_numbers_times_2(numbers_list):
 def check_if_lists_have_an_equal(list_a, list_b):
 	for element_a in list_a:  #O(n)
 		for element_b in list_b:   # O(n^2)
-			if element_a == element_b: #O(1)
-				return True             #O(1)
+			if element_a == element_b: # O(n^2)
+				return True             # O(n^2)
 				
 	return False                        #O(1)
 
@@ -26,10 +26,10 @@ def check_if_lists_have_an_equal(list_a, list_b):
 
 def print_10_or_less_elements(list_to_print):
 	list_len = len(list_to_print)    #O(1)  Len is constant
-	for index in range(min(list_len, 10)):   #O(1)  range up to 10
-		print(list_to_print[index])  #O(1)
+	for index in range(min(list_len, 10)):   #O(10)  range up to 10
+		print(list_to_print[index])  #O(10)
 
-#Worst scenario  O(1)
+#Worst scenario  O(10)
 #Best scenario   O(1)
 
 #generate_list_trios
@@ -37,9 +37,9 @@ def print_10_or_less_elements(list_to_print):
 def generate_list_trios(list_a, list_b, list_c):
 	result_list = []   #O(1)
 	for element_a in list_a:  #O(n)
-		for element_b in list_b: #O(n)
-			for element_c in list_c: #O(n)
-				result_list.append(f'{element_a} {element_b} {element_c}') #O(1)
+		for element_b in list_b: # O(n^2)
+			for element_c in list_c: # O(n^3)
+				result_list.append(f'{element_a} {element_b} {element_c}') # O(n^3)
 				
 	return result_list    #O(1)
 		
