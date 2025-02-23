051bffd2af41124346ee56935e2008211f264d29
Darwin Oporta <oportadarwin24@gmail.com>
2025-02-23 17:36:54 -0600

    updated project

---
+++ Semana17_proyecto/lists.py
@@ -0,0 +1,7 @@
+def create_list_of_categories():
+    list_of_categories=[]
+    return list_of_categories
+
+def create_list_of_income_and_expenses():
+    list_of_income_and_expenses=[]
+    return list_of_income_and_expenses
---
+++ Semana17_proyecto/table.py
@@ -0,0 +1,42 @@
+import PySimpleGUI as sg
+
+
+def print_list_of_transactions(list_of_categories, list_of_income_and_expenses):
+    list_of_items= [item["category"] for item in list_of_categories]
+    values_of_table=[[transaction["category"], transaction["type"], transaction["title"], transaction["amount"]] for transaction in list_of_income_and_expenses if isinstance(transaction, dict)]
+    headings_of_table= ["Category", "Type", "Title", "Amount"]
+    layout_1 = [
+        [sg.Text("TABLE OF TRANSACTIONS", font=("Helvetica", 20), justification="center", expand_x=True)],
+        [sg.Table(values=values_of_table,
+        headings=headings_of_table,
+        auto_size_columns=True, 
+        display_row_numbers=False,
+        selected_row_colors="blue on yellow",
+        justification="center")]
+
+
+    ]
+
+    layout_2= [
+        [sg.Text("CATEGORIES", font=("Helvetica", 20), justification="center", expand_x=True)],
+        [sg.Table(values=[[category] for category in list_of_items], headings=["Categories"], 
+        auto_size_columns=True, 
+        display_row_numbers=False, 
+        selected_row_colors="blue on yellow", 
+        justification="center")]
+
+    ]
+
+    tabs = sg.TabGroup([[sg.Tab("Transactions",layout_1),
+        sg.Tab("Categories", layout_2)],
+        [sg.Button("Return")]
+        ], tab_location="centertop")
+
+    window= sg.Window("TABLE OF TRANSACTIONS", [[tabs]], resizable=True)
+
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Exit" or event == "Return":
+            break
+
+    window.close()
